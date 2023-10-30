ea1cd5f3caed311aa23d51c864b74ad8e2cd1f8d
mohammedaseebk <mdazeeb432@gmail.com>
2023-10-30 11:29:02 +0530

    netflix

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
 import './App.css';
+import Row from './components/Row';
+import Banner from './components/Banner';
+import Nav from './components/Nav';
+
+import requests from './requests';
 
 function App(){
   return(
       
        <div>
-        <h1 style={{color:'red'}} >Netflix</h1>
+        <Nav/>
+
+        <Banner fetchUrl={requests.fetchNetflixOriginals}/>
+
+        <Row isPresent={true}  title="Trending" fetchUrl={requests.fetchTrending}/>
+
+        <Row title="NetflixOriginals" fetchUrl={requests.fetchNetflixOriginals}/>
+
+
+        <Row title="Top Rated" fetchUrl={requests.fetchTopRated}/>
+
+        <Row title="Action Movies" fetchUrl={requests.fetchActionMovies}/>
+
+
+        <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies}/>
+
+        <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies}/>
+
+
+        <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies}/>
+
+        <Row title="Documenteries" fetchUrl={requests.fetchDocumentaries}/>
+
+
        </div>
       
   );
